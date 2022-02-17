1477539c0d98ae6f927e90d710b9d27e702082fa
Hossein <hosseinjarrahi@gmail.com>
2022-02-17 15:53:42 +0330

    alpha version

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import MForm from "./src/components/MForm.vue";
 import mitt from "./src/plugins/mitt";
+import FormFieldAbstract from "./src/components/form/FormFieldAbstract.vue";
 
-export { MForm, mitt };
+export { MForm, mitt, FormFieldAbstract };
