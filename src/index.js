254b6d9c34138dc12254950e74657bc1f3b76b24
Hossein <hosseinjarrahi@gmail.com>
2022-06-09 12:30:46 +0430

    fix bug export index

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import useEmitter from "./composables/useEmitter";
 import FormFieldAbstract from "./components/form/FormFieldAbstract.vue";
 import useFieldProps from "./composables/useFieldProps";
 
-export default { MForm, useEmitter, FormFieldAbstract, useFieldProps };
+export default MForm;
+
+export { useEmitter, FormFieldAbstract, useFieldProps };
